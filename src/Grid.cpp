ef575d88f69a6367ceba701913efbdaf3e8a4b97
rmcm <rmcm@g5c29.cin.ufpe.br>
2025-11-12 17:11:34 -0300

    fix syntax error

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -275,7 +275,7 @@ void Grid::multiplyByLaplacian2(Vector &firstComponent) {
     VECTOR_TYPE newFirstComponent[numberOfVectors];
 
     float horizontalCotangentWeight = m_delta_x / m_delta_y;
-    float verticalCotangentWeight = / m_delta_x;
+    float verticalCotangentWeight = m_delta_y / m_delta_x;
 
     //numberOfVectors == numberOfVertices in the grid == resolution * resolution
     for(int i = 0 ; i < numberOfVectors ; ++i){
@@ -361,7 +361,7 @@ void Grid::multiplyByLaplacian2(Vector &firstComponent) {
                 coef += horizontalCotangentWeight;
             }
             if(canMoveRight){
-                coef + = horizontalCotangentWeight;
+                coef += horizontalCotangentWeight;
             }
 
             coef /= 2.0;
